6ca79a2d29dd752cf748b1a1f4cd700eab5c60c3
tarnacious <tarn@tarnbarford.net>
2019-10-09 00:37:45 +0200

    initial configuration

---
+++ configuration.nix
@@ -0,0 +1,91 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ 
+    ./hardware-configuration.nix
+  ];
+
+  networking.extraHosts =
+    ''
+    '';
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 16384;
+    }
+  ];
+
+  networking.hostName = "xps-nixos"; 
+  networking.networkmanager.enable = true;  
+
+  security.pam.services.gdm.enableGnomeKeyring = true;
+
+  virtualisation.docker.enable = true;
+  virtualisation.libvirtd.enable = true;
+
+  hardware.nitrokey.enable = true;
+
+  i18n = {
+    consoleFont = "latarcyrheb-sun32";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  time.timeZone = "Europe/Berlin";
+
+  environment.systemPackages = with pkgs; [
+    wget 
+    (import ./vim.nix)
+    gawk
+    python
+    owncloud-client
+    nitrokey-app
+    chromium
+    firefox
+    pass
+    git 
+    gnupg
+    neomutt
+    offlineimap
+    msmtp
+    docker_compose
+    fzf
+    tmux
+    cryptsetup
+    ccrypt
+    dhcpcd
+    bind
+    file
+    htop
+    ack
+    lynx
+  ];
+
+  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  programs.adb.enable = true;
+
+  services.printing.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.desktopManager.gnome3.enable = true;
+
+  services.xserver.libinput.enable = true;
+
+  users.users.tarn = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "adbusers" ];
+  };
+
+  system.stateVersion = "19.03";
+}
---
+++ vim.nix
@@ -0,0 +1,122 @@
+with import <nixpkgs> {};
+
+vim_configurable.customize {
+    # Specifies the vim binary name.
+    # E.g. set this to "my-vim" and you need to type "my-vim" to open this vim
+    # This allows to have multiple vim packages installed (e.g. with a different set of plugins)
+    name = "vim";
+    vimrcConfig.customRC = ''
+    set nocompatible
+    filetype plugin on
+    syntax on
+    filetype indent on
+    syntax enable
+    set wildmenu
+    set hid
+    set showmatch
+
+    " No sound on errors
+    set noerrorbells
+    set novisualbell
+    set t_vb=
+    set tm=500
+
+    " don't confuse webpack reloader
+    set backupcopy=yes
+
+    set tabstop=2
+    set shiftwidth=2
+    set expandtab
+    set nu
+
+    let mapleader = ","
+
+    nnoremap <leader>y "+y
+    nnoremap <leader>p "+p
+
+    vnoremap <leader>y "+y
+    vnoremap <leader>p "+p
+
+    " Mappings for finding files with fzf
+    nmap ; :Buffers<CR>
+    nmap <Leader>t :Files<CR>
+    nmap <Leader>r :Tags<CR>
+
+
+    nnoremap <leader>av ggVG
+    nnoremap <leader>ev :vsplit $MYVIMRC<cr>
+    nnoremap <leader>sv :source $MYVIMRC<cr>
+    nnoremap <leader>ag :Ack<cr>
+    nnoremap <leader>sp :setlocal spell spelllang=en_au<cr>
+    nnoremap <leader>ct :!ctags -R .<cr>
+    nnoremap <leader>nt :NERDTree<cr>
+    nnoremap <leader>at :ALEToggle<CR>
+
+    inoremap jk <esc>
+
+    "Move line by paragraph
+    nnoremap j gj
+    nnoremap k gk
+    vnoremap j gj
+    vnoremap k gk
+
+    "Tab-style buffer switching
+    map <C-S-tab> :bprev<CR>
+    map <C-tab> :bnext<CR>
+
+    set clipboard=unnamed
+
+    let g:slime_target = "tmux"
+
+    "Strip trailing white space
+    fun! <SID>StripTrailingWhitespaces()
+        let l = line(".")
+        let c = col(".")
+        %s/\s\+$//e
+        call cursor(l, c)
+    endfun
+
+    autocmd FileType c,cpp,java,php,ruby,python,coffee,js,javascript,clojure,ld,s,html autocmd BufWritePre <buffer> :call <SID>StripTrailingWhitespaces()
+    autocmd FileType ruby setlocal expandtab shiftwidth=2 tabstop=2 softtabstop=2
+
+    autocmd Filetype scss setlocal ts=2 sts=2 sw=2
+
+    let g:ale_fixers = {'javascript': ['prettier_standard']}
+
+    filetype plugin indent on
+    syntax on
+
+    if executable('ag')
+      let g:ackprg = 'ag --vimgrep'
+    endif
+
+
+    syntax enable
+    set background=dark
+    " colorscheme solarized
+
+    let NERDTreeIgnore = ['\.pyc$', '\.so$', '\.swp$']
+
+    map <leader>jr :set makeprg=rubocop <CR>:make % %:r<CR>:copen<CR>
+    '';
+
+    vimrcConfig.packages.myVimPackage = with pkgs.vimPlugins; {
+      # loaded on launch
+      start = [ 
+        youcompleteme 
+        fugitive 
+        nerdtree 
+        vim-ruby
+        vim-puppet
+        fzf
+        fzf-vim
+        fzfWrapper
+        vim-colors-solarized
+        ale
+      ];
+      # manually loadable by calling `:packadd $plugin-name`
+      opt = [];
+      # To automatically load a plugin when opening a filetype, add vimrc lines like:
+      # autocmd FileType php :packadd phpCompletion
+    };
+}
